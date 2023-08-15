27720f228eec1181720a23f27646034ccd2f08c1
anusha <arora.anusha@northeastern.edu>
2023-08-15 15:09:10 -0400

    debug

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,34 +1,63 @@
+// Import necessary modules and dependencies
 import * as tuitsDao from "./tuits-dao.js";
 
+// Define the createTuit function
 const createTuit = async (req, res) => {
-  const newTuit = req.body;
-  newTuit.likes = 0;
-  newTuit.liked = false;
-  newTuit.disliked = false;
-  newTuit.dislikes = 0;
-  newTuit.replies = 0;
-  newTuit.retuits = 0;
-  const insertedTuit = await tuitsDao.createTuit(newTuit);
-  res.json(newTuit);
+  try {
+    const newTuit = req.body;
+    // Initialize properties
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    newTuit.disliked = false;
+    newTuit.dislikes = 0;
+    newTuit.replies = 0;
+    newTuit.retuits = 0;
+    
+    // Create a new tuit using the dao function
+    const insertedTuit = await tuitsDao.createTuit(newTuit);
+    res.json(insertedTuit); // Return the newly created tuit
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while creating the tuit." });
+  }
 };
+
+// Define the findTuits function
 const findTuits = async (req, res) => {
-  const tuits = await tuitsDao.findTuits();
-  res.json(tuits);
+  try {
+    const tuits = await tuitsDao.findTuits();
+    res.json(tuits); // Return the list of tuits
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching tuits." });
+  }
 };
 
+// Define the updateTuit function
 const updateTuit = async (req, res) => {
-  const tuitdIdToUpdate = req.params.tid;
-  const updates = req.body;
-  const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
-  res.json(status);
+  try {
+    const tuitIdToUpdate = req.params.tid;
+    const updates = req.body;
+    
+    // Update the tuit using the dao function
+    console.log(updates);
+    const status = await tuitsDao.updateTuit(tuitIdToUpdate, updates);
+    res.json(status);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while updating the tuit." });
+  }
 };
 
+// Define the deleteTuit function
 const deleteTuit = async (req, res) => {
-  const tuitdIdToDelete = req.params.tid;
-  const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
-  res.json(status);
+  try {
+    const tuitIdToDelete = req.params.tid; // Fix typo here
+    const status = await tuitsDao.deleteTuit(tuitIdToDelete); // Fix typo here
+    res.json(status);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while deleting the tuit." });
+  }
 };
 
+// Export routes for use in your app
 export default (app) => {
   app.post("/api/tuits", createTuit);
   app.get("/api/tuits", findTuits);
